6aa5d312793f1c3beb9c2806c24bd9c583fbfea0
flekynn <marcos.alcaraz814@gmail.com>
2025-08-31 01:42:21 -0300

    Update index.js
    ver 2.1 ahora si definitiva

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ async function checkStreams() {
                 .setThumbnail(stream.thumbnail_url.replace('{width}', '320').replace('{height}', '180'));
 
             await channel.send({
-        content: '@1411569893014241282',
+        content: '@&1411569893014241282',
         embeds: [embed]
     });
             twitchLive = true;
@@ -127,7 +127,7 @@ async function checkStreams() {
                     .setDescription(stream.title || 'Transmisión en vivo');
 
                 await channel.send({
-        content: '@1411569893014241282',
+        content: '@&1411569893014241282',
         embeds: [embed]
     });
                 kickLive = true;
