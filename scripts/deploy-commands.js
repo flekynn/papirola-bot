88d7754d13a4fb9161fa7b0e96ccc38d09f212f8
Marcos Alcaraz <marcos.alcaraz814@gmail.com>
2025-11-18 21:44:43 -0300

    cambios final v3

--- scripts/deploy-commands.js
+++ scripts/deploy-commands.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const commands = [];
-const commandsPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(__dirname, '..', 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
